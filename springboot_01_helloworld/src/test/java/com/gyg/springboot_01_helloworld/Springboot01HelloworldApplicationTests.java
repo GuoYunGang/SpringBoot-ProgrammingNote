ac2606742304e4d397e6036c37eb48118528ec7e
GuoYunGang <64789312+GuoYunGang@users.noreply.github.com>
2021-10-08 19:22:34 +0800

    Add files via upload

---
+++ springboot_01_helloworld/src/test/java/com/gyg/springboot_01_helloworld/Springboot01HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyg.springboot_01_helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
